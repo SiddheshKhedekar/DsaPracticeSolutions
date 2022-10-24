0152712141e81b82ca1c6d1177fcae48be4207fa
Siddhesh Khedekar <siddheshkhedekar.dev@gmail.com>
2022-10-24 17:14:14 +0530

    add explanation for FixNamesinaTable solution

--- LeetCode/SQL/FixNamesinaTable/script.sql
+++ LeetCode/SQL/FixNamesinaTable/script.sql
@@ -1,4 +1,9 @@
 -- Fix Names in a Table -> MySql
+
+/*
+Explanation: As per intution I needed to ensure the first character was in upper case and the rest of string was lower case. The solution I implemented made use of upper and lower functions for this purpose. I used the substring function to seperate the two parts of the string that I wanted in lowercase and uppercase and concatenated them together again. Thing to remember is that substring starts from 1.
+*/
+
 select 
   user_id, 
   concat(
