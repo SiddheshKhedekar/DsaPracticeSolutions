1fe318c0fd50c11218a4bf22b81fbafe47ade00e
Siddhesh Khedekar <siddheshkhedekar.dev@gmail.com>
2022-10-24 17:58:37 +0530

    update explanation of UniquePaths

--- LeetCode/DSA/UniquePaths/script.py
+++ LeetCode/DSA/UniquePaths/script.py
@@ -1,4 +1,8 @@
-from math import factorial
+# Unique Paths -> Python3
+
+'''
+Explanation: In a m X n matrix we can try to solve it as a math combinatorics problem. We understand we need to make m-1 right moves and n-1 down mover to reach finish from the start out of total m+n-2 moves. Considering all different combinations we use the factorial of m+n-2 and floor divide by factorial of m-1 and n-1.
+'''
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
