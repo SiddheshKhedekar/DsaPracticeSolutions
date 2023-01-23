e51579253085c83f78edf519acf62271b048f929
Siddhesh Khedekar <siddheshkhedekar.dev@gmail.com>
2023-01-23 19:32:54 +0530

    fix code formatting

--- LeetCode/DSA/CountandSay/script.py
+++ LeetCode/DSA/CountandSay/script.py
@@ -8,6 +8,7 @@ string using join a combination of group and digit which we can get after groupb
 class Solution:
     def countAndSay(self, n: int) -> str:
         s = '1'
-        for _ in range(n-1): s = ''.join(str(len(list(group))) + digit 
-        for digit, group in itertools.groupby(s))
+        for _ in range(n-1): 
+            s = ''.join(str(len(list(group))) + digit 
+            for digit, group in itertools.groupby(s))
         return s
