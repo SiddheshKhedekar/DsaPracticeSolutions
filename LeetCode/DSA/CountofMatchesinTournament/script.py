84274b5eb0fa2eff87562a5b84c02689a43fb4da
Siddhesh Khedekar <siddheshkhedekar.dev@gmail.com>
2023-12-06 21:58:22 +0530

    add CountofMatchesinTournament problem solution

---
+++ LeetCode/DSA/CountofMatchesinTournament/script.py
@@ -0,0 +1,8 @@
+# Count of Matches in Tournament -> Python3
+
+'''
+Explanation: On observation it is clear to return n -1 in all cases.
+'''
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int: return n - 1
